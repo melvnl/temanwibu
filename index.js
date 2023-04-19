9423184c16c2c4aa07ed6da6c6b140649d461452
melvnl <melvinlim2000@gmail.com>
2023-04-19 16:48:07 +0700

    fix: set Asia/Jakarta as node-cron timezone, update new interval

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ import { getTitle } from "./lib/scrape/index.js";
 import { config } from "dotenv";
 
 config();
-// Start the scraping
-// NOTE : https://crontab.guru/#0_18_*_*_*
-const job = nodeCron.schedule("0 18 * * *", getTitle);
+// NOTE : https://crontab.guru/#0_21_*_*_*
+// NOTE TIMEZONE : https://raw.githubusercontent.com/node-cron/tz-offset/master/generated/offsets.json
+const job = nodeCron.schedule("0 21 * * *", getTitle,
+  {
+    scheduled: true,
+    timezone: "Asia/Jakarta"
+  });
 
 job.start();
